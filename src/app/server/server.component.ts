147109150faefc3a1167b89ca3b87c5836d7b7ad
spencer <spencer.powell@me.com>
2019-10-23 16:13:31 -0600

    End of 22 with my own additions

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
-}) //pass javascript object to configure comp
+}) // pass javascript object to configure comp
 
 export class ServerComponent {
-    
+
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,17 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <h3>Hello World</h3>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <h3>Hello World</h3>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+
+  constructor() {
+    setTimeout(() => this.allowNewServer = true, 3000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    if (this.serverName !== '') {
+      this.serverCreationStatus = `Server '${this.serverName}' was created.`;
+    } else {
+      this.serverCreationStatus = 'Please enter a valid server name.';
+    }
+    setTimeout(() => {
+      this.serverCreationStatus = 'Waiting...';
+      this.serverName = '';
+    }, 5000);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = ( event.target as HTMLInputElement ).value; // this adds explicit type to event.target
   }
 
 }
